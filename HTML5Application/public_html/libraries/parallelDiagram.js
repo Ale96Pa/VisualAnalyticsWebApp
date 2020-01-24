4a0bd70b407558f3d8d7dbfa72aae429a42919d7
Alessandro <Alessandro@10.90.19.126>
2020-01-24 11:43:00 +0100

    Parallel coordinates initialization

---
+++ HTML5Application/public_html/libraries/parallelDiagram.js
@@ -0,0 +1,10 @@
+/* 
+ * Script for the management of the parallel diagram containing the parameters
+ * (Sex, Age, Generation)
+ */
+
+function drawParallelCoordinates(){  
+ alert("Hello Javatpoint");  
+}  
+
+
